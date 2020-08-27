d88c6451028dd8da42a2a7382b838dd9ad28aaae
liangcg <liangcg@east-sw.com>
2020-08-27 21:57:33 +0800

    init commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -20,83 +20,78 @@
 		data() {
 			return {
 				msg: "this is my webpack home!1!!1",
-				mySocket:null,
-				count:0,
-				showOrNot:true,
+				mySocket: null,
+				count: 0,
+				showOrNot: true,
 			};
 		},
-		directives:{
-			focus:{
-				bind(el,binding,vnode,oldVnode){
+		directives: {
+			focus: {
+				bind(el, binding, vnode, oldVnode) {
 					// console.log(el.parentNode)
 					console.log("bind")
 					console.log(binding)
 					el.style.background = "red"
 				},
-				inserted(el,binding,vnode,oldVnode){
+				inserted(el, binding, vnode, oldVnode) {
 					// console.log(el.parentNode)
 					console.log("inserted")
 					el.focus()
 				},
-				update(el,binding,vnode,oldVnode){
+				update(el, binding, vnode, oldVnode) {
 					console.log("update")
 					el.focus()
 				},
-				componentUpdated(el,binding,vnode,oldVnode){
+				componentUpdated(el, binding, vnode, oldVnode) {
 					console.log("componentUpdated")
 					el.focus()
 				},
-				unbind(el,binding,vnode,oldVnode){
+				unbind(el, binding, vnode, oldVnode) {
 					console.log("unbind")
 					el.focus()
 				}
 			}
 		},
-		filters:{
-			qqq(val){
+		filters: {
+			qqq(val) {
 				return val / 100
 			}
 		},
 		created() {
-			if ("WebSocket" in window)
-			{
+			if ("WebSocket" in window) {
 				// alert("您的浏览器支持 WebSocket!");
 
 				// 打开一个 web socket
 				var ws = this.mySocket = new WebSocket("ws://localhost:3000");
 
-				ws.onopen = function()
-				{
+				ws.onopen = function () {
 					// Web Socket 已连接上，使用 send() 方法发送数据
 					ws.send("发送数据");
 					// alert("数据发送中...");
 				};
 
-				ws.onmessage = function (evt)
-				{
+				ws.onmessage = function (evt) {
 					var received_msg = evt.data;
 					// alert("数据已接收...");
 				};
 
-				ws.onclose = function()
-				{
+				ws.onclose = function () {
 					// 关闭 websocket
 					// alert("连接已关闭...");
 				};
 			}
 
-			else
-			{
+			else {
 				// 浏览器不支持 WebSocket
 				// alert("您的浏览器不支持 WebSocket!");
 			}
 		},
 		methods: {
-			haha(){
-				this.$router.push({path:"/test"})
+			haha() {
+				this.$router.push({path: "/test"})
 				// history.pushState(null, null, '/test');
 			},
-			qwe(){
+			qwe() {
 				this.mySocket.send("是我是我~")
 			}
 		}
